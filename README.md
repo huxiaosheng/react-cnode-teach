1f23f8f2bf6cb9132a1482b0b13de74151245400
huxiaosheng <hxs15692194802@gmail.com>
2018-04-16 01:21:18 +0800

    first commit

--- README.md
+++
@@ -1,9 +0,0 @@
-# 一个react+webpack 的项目
-
-### 目录结构
-
-|— build
-
-|— client
-
-|— 
--- client/App.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'   //eslint-disable-line
-
-export default hot(module)(() => <div>this is a app</div>)
